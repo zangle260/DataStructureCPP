2916ce22d4e29346e68b49f9c6df1b7653547050
zhaogang <zhaogang260@126.com>
2020-03-27 16:36:14 +0800

    添加堆栈和队列

---
+++ DataStructure/Queue.h
@@ -0,0 +1,9 @@
+#pragma once 
+
+#include "List.h" //以List为基类
+template <typename T> class Queue : public List<T> { //队列模板类（继承List原有接口）
+public: //size()、 empty()以及其它开放接口均可直接沿用
+    void enqueue(T const& e) { insertAsLast(e); } //入队：尾部揑入
+    T dequeue() { return remove(first()); } //出队：首部初除
+    T& front() { return first()->data; } //队首    
+};
